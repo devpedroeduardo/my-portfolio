0f095c3508a51a7d4fc476026ef553488ebbb11d
Pedro Eduardo <pedroedu77@gmail.com>
2025-12-19 09:31:14 -0300

    chore: prepare for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [
     tailwind(), 
     react()
-  ]
+  ],
+
+  adapter: vercel()
 });
